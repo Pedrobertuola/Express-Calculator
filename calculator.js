4b34c62ed642f896a30f3e0e6768947cd72d0dc8
PedroBertuola <pedrobertuola@gmail.com>
2022-08-21 10:22:02 -0300

    BMI added

--- calculator.js
+++ calculator.js
@@ -11,6 +11,10 @@ app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')
 })
 
+app.get('/bmicalculator', function(req, res) {
+    res.sendFile(__dirname + '/bmicalculator.html')
+})
+
 app.post("/", function(req, res) {
     var num1 = Number(req.body.num1);
     var num2 = Number(req.body.num2);
@@ -19,6 +23,16 @@ app.post("/", function(req, res) {
 
     res.send("The result of the calculation is " + result);
 });
+
+app.post("/bmicalculator", function(req, res) {
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var result = weight/(height*height);
+    
+
+    res.send("Your BMI is:  "+ result)
+})
 
 
 app.listen(port, () => {
